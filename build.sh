b9572f7a89eb9a261a49d917928f429c1a8a491b
Dan Cinnamon <42472957+dancinnamon-okta@users.noreply.github.com>
2024-02-12 18:24:44 -0600

    Added a new container that runs the scimgateway component

--- build.sh
+++ build.sh
@@ -10,8 +10,23 @@ openssl req \
   -keyout sdk_demo.key \
   -out sdk_demo.cert
 
+openssl req \
+  -new \
+  -newkey rsa:4096 \
+  -days 3650 \
+  -nodes \
+  -x509 \
+  -subj "/C=US/ST=CA/L=SF/O=demo/CN=scimgateway" \
+  -keyout scimgateway.key \
+  -out scimgateway.cert
+
 openssl pkcs12 -export -in sdk_demo.cert -inkey sdk_demo.key -out sdk_demo.p12 -password pass:$KEYSTORE_PASSWORD -name demossl
+openssl pkcs12 -certpbe AES-256-CBC -keypbe AES-256-CBC -export -in scimgateway.cert -inkey scimgateway.key -out scimgateway.p12 -password pass:$KEYSTORE_PASSWORD -name demossl
+
 mv ./sdk_demo.cert ./container_opp_agent
+mv ./scimgateway.cert ./container_opp_agent
+
 mv ./sdk_demo.p12 ./container_sdk_demo
+mv ./scimgateway.p12 ./container_scimgateway
 
 docker compose build --no-cache
